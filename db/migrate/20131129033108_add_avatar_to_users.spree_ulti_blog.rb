67e2bb86cdaddc722f4ca19b75c0d46688ec88f6
Freon26 <xexo26@yahoo.com.ph>
2013-11-30 18:19:02 -0800

    Modified index.html.erb for front page 2

--- db/migrate/20131129033108_add_avatar_to_users.spree_ulti_blog.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from spree_ulti_blog (originally 20130906113056)
-class AddAvatarToUsers < ActiveRecord::Migration
-  def change
-    add_column :spree_users, :avatar_file_name, :string
-    add_column :spree_users, :avatar_content_type, :string
-    add_column :spree_users, :avatar_file_size, :string
-  end
-end
--- db/migrate/20131129033109_add_avatar_to_blog_posts.spree_ulti_blog.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from spree_ulti_blog (originally 20130909134225)
-class AddAvatarToBlogPosts < ActiveRecord::Migration
-  def change
-    add_column :spree_blog_posts, :avatar_file_name, :string
-    add_column :spree_blog_posts, :avatar_content_type, :string
-    add_column :spree_blog_posts, :avatar_file_size, :integer
-  end
-end
